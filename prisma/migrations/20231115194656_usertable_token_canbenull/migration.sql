d2b86058b41aba581630dc4dc806e5b5e25391b0
Luis Cobian <luigui.angel07@hotmail.com>
2023-11-15 19:00:14 -0600

    init usertable

---
+++ prisma/migrations/20231115194656_usertable_token_canbenull/migration.sql
@@ -0,0 +1,13 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "token" TEXT
+);
+INSERT INTO "new_User" ("email", "id", "password", "token") SELECT "email", "id", "password", "token" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
